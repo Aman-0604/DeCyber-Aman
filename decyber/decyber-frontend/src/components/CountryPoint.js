e76be13fa667b57780487440e42494c01e7b0b95
Aditya Mishra <adityavishu03@gmail.com>
2023-01-31 20:42:15 +0530

    Added Submit and Clear text buttons

--- decyber/decyber-frontend/src/components/CountryPoint.js
+++ decyber/decyber-frontend/src/components/CountryPoint.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import "../styles/countrypoint.css"
 
 const CountryPoint = () => {
   let { country } = useParams();
+  const [text, setText] = useState("")
+  // let textval;
+  const onChange =(e)=>{
+    setText(e.target.value)
+    // console.log(e.target.value)
+    // textval = e.target.value;
+  }
+  const clearText = ()=>{
+    setText("");
+  }
+  const handleSubmitClick = ()=>{
+    setText(text)
+  }
   return (
     <div className="CountryPoint">
       <div className='cp-content d-flex justify-content-center align-items-center'>
@@ -14,7 +27,14 @@ const CountryPoint = () => {
               <p className="points-pallete">Pts. 500</p>
             </div>
           </div>
-          <textarea name="answer" id="answer" rows="5" placeholder='Write your answer here'></textarea>
+          <textarea name="answer" id="answer" rows="5" value={text} onChange={onChange} placeholder='Write your answer here'></textarea>
+          <div style={{width:"75%",marginTop:".5rem"}}>
+          <button className="btn btn-danger me-3" style={{width:"40%"}} onClick={clearText}>Clear</button>
+          <button className="btn btn-success" style={{width:"40%"}} onClick={()=>{
+            handleSubmitClick();
+            clearText();
+            }} >Submit</button>
+          </div>
         </div>
       </div>
     </div>
